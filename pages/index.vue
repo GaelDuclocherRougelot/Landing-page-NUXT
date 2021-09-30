6c460d8341d388bce2346ff98b4eb233a9f6d8f4
GaelDRougelot <75973371+GaelDRougelot@users.noreply.github.com>
2021-09-30 11:13:47 +0200

    responsive bug fix, add a logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div class="section1">
-      <span>LOGO</span>
+      <img id="logo" src="~/assets/logo-agence-voyage.png" alt="">
       <div class="title">
         <h1>
           Voyager n'a jamais été aussi simple avec
@@ -14,7 +14,7 @@
       </div>
     </div>
 
-    <div  id="section2" class="section2">
+    <div id="section2" class="section2">
       <div class="gallery">
         <figure class="gallery__item gallery__item--1">
           <img src="~/assets/plagejpg.jpg" class="gallery__img" alt="Image 1" />
@@ -37,25 +37,43 @@
       <div id="section3">
         <div class="top">
           <h1>Nos services</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, aperiam! Commodi itaque necessitatibus autem asperiores! Minus natus dicta modi. Eius.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+            aperiam! Commodi itaque necessitatibus autem asperiores! Minus natus
+            dicta modi. Eius.
+          </p>
           <div id="separator"></div>
         </div>
         <div class="bottom">
           <div class="card">
-            <img src="~/assets/work.jpg" alt="">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, beatae.</p>
+            <img src="~/assets/work.jpg" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi,
+              beatae.
+            </p>
           </div>
           <div class="card">
-            <img src="~/assets/work.jpg" alt="">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur laboriosam possimus debitis veritatis consectetur facilis!</p>
+            <img src="~/assets/work.jpg" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur laboriosam possimus debitis veritatis consectetur
+              facilis!
+            </p>
           </div>
           <div class="card">
-            <img src="~/assets/work.jpg" alt="">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, beatae.</p>
+            <img src="~/assets/work.jpg" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi,
+              beatae.
+            </p>
           </div>
         </div>
       </div>
 
+      <!-- <div id="section4">
+      <h1>Rejoindre l'aventure ?</h1>
+      <button>Voyager dès maintenant</button>
+    </div> -->
     </div>
   </div>
 </template>
@@ -65,15 +83,15 @@ export default {}
 </script>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@200;300;400;500&display=swap');
-a{
+a {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   margin-right: 100px;
 }
 .home {
-  width: 100%;
-  height: 100%;
+  /* width: 100%;
+  height: 100%; */
   display: flex;
   align-items: center;
   flex-direction: column;
@@ -91,55 +109,63 @@ a{
     contrast(120%);
 }
 
-.section2 {
-  width: 100%;
-  height: 100vh;
+#logo {
+  width: 50px;
+  position: absolute;
+  top: 30px;
+  left: 30px;
 }
 
-.gallery{
+.section2 {
+  margin: 5px;
+  width: 100%;
+  height: 100%;
+}
+
+.gallery {
   margin: 10px;
 }
 
-.section2>img {
+.section2 > img {
   max-width: 100%;
   height: auto;
 }
 
-.gallery__item{
+.gallery__item {
   height: auto;
   margin: 0.5rem;
 }
 
-.gallery__item img{
+.gallery__item img {
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
 
-@media  (min-width: 640px){
-  .gallery{
+@media (min-width: 640px) {
+  .gallery {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(4, 300px);
+    grid-template-rows: repeat(4, 240px);
     grid-auto-flow: row dense;
   }
-  .gallery__item{
+  .gallery__item {
     margin: 0.7rem;
   }
 
-  .gallery__item:first-child{
+  .gallery__item:first-child {
     grid-row: span 2;
   }
-  .gallery__item:nth-child(2){
+  .gallery__item:nth-child(2) {
     grid-column: 1;
     grid-row: span 2;
   }
   .gallery__item:nth-child(6),
-  .gallery__item:nth-child(3){
+  .gallery__item:nth-child(3) {
     grid-row: span 2;
   }
 
-  .gallery__item:nth-child(4){
+  .gallery__item:nth-child(4) {
     grid-row: span 2;
   }
 }
@@ -176,14 +202,16 @@ span {
   top: 20px;
   left: 20px;
 }
+@media (max-width: 1500px) {
+  #bg-img {
+    filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(-20deg)
+      brightness(500%) contrast(200%);
+  }
+}
 
 @media (max-width: 770px) {
   h2 {
     color: white;
-  }
-  #bg-img {
-    filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(-20deg)
-      brightness(500%) contrast(200%);
   }
   .section1 {
     align-items: flex-start;
@@ -195,7 +223,8 @@ span {
 
 /* Section 3 services */
 
-#section3{
+#section3 {
+  position: relative;
   width: 100%;
   height: 100vh;
   display: flex;
@@ -204,17 +233,17 @@ span {
   /* background-color: whitesmoke; */
 }
 
-.top{
+.top {
   display: flex;
   flex-direction: column;
   text-align: center;
   align-items: center;
   width: 500px;
   height: 25%;
-  margin-top: 100px;
+  margin-top: 40px;
 }
 
-p{
+p {
   margin-top: 10px;
   font-family: 'Quicksand', sans-serif;
   font-size: 18px;
@@ -226,37 +255,74 @@ p{
   height: 2px;
   background-color: rgb(0, 162, 255);
   margin-top: 20px;
+  margin-bottom: 20px;
 }
 
-.bottom{
+.bottom {
   display: flex;
-  width: 80%;
-  height: 650px;
   align-items: center;
+  justify-content: center;
+  width: 100%;
 }
 
-.card{
-  width: 600px;
-  height: 600px;
+.card {
+  width: 500px;
+  height: 500px;
   margin: 20px;
 }
 
-.card>img{
-  width: 100%;
-  object-fit: cover;
+.card > img {
   padding: 10px;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
-@media  (max-width:860px) {
-  .bottom{
-    flex-direction: column;
+@media (max-width: 860px) {
+  .bottom {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-template-rows: repeat(3, 400px);
+    grid-auto-flow: row dense;
   }
-  .card{
+  .card {
+    width: 350px;
+    margin: 0 auto;
+    height: 100%;
+  }
+  .card > img {
+    height: 70%;
+  }
+  .top {
     width: 375px;
   }
-  .top{
-    width: 375px;
+  #section3 {
+    height: 100%;
   }
 }
 
+/* Section 4 join us */
+
+#section4 {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#section4 > button {
+  padding: 20px;
+  font-size: 20px;
+  border: none;
+  background-color: rgb(75, 204, 255);
+  border-radius: 50px;
+  width: 300px;
+  transition: all 1s ease;
+}
+#section4 > button:hover {
+  transition: all 1s ease;
+}
 </style>
