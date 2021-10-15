b0e8ecd75fb1ac195a1e681de84629acc6c121f5
Gaël <gaelduclocher.rougelot@gmail.com>
2021-10-15 16:22:04 +0200

    organize assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="home">
     <div class="section1 relative">
-      <img class="backgrounds z-0 absolute object-cover w-full h-screen" src="~/assets/asia-min.jpg" alt="">
-      <img class="logo w-14 absolute top-8 left-6" src="~/assets/logo-agence-voyage.png" alt="" />
-        <button class="absolute top-8 right-6 md:hidden lg:flex text-lg text-white bg-red-500 pl-5 pr-5 pt-3 pb-3 rounded-lg flex ease duration-75">
+      <img class="backgrounds z-0 absolute object-cover w-full h-screen" src="~/assets/jpgs/asia-min.jpg" alt="background">
+      <img class="logo w-14 absolute top-8 left-6" src="~/assets/pngs/logo-agence-voyage.png" alt="logo" />
+        <button class="absolute top-8 right-6 sm:hidden lg:flex text-lg text-white bg-red-500 pl-5 pr-5 pt-3 pb-3 rounded-lg flex ease duration-75">
           <img class="mr-1" src="https://img.icons8.com/external-prettycons-flat-prettycons/30/000000/external-send-social-media-prettycons-flat-prettycons.png"/>
           Réserver
         </button>
       <div class="title w-full flex flex-col text-center lg:justify-start lg:items-start lg:text-left lg:ml-52">
-        <h1 v-parallax="-0.2" class="text-white text-5xl lg:text-5xl 2xl:text-6xl font-normal tracking-tighter leading-snug">
+        <h1 v-parallax="-0.2" class="text-white text-5xl lg:text-5xl 2xl:text-6xl font-normal tracking-tighter leading-snug">                     
           Voyager n'a jamais été aussi simple avec
         <h2 class="text-white font-light text-4xl lg:text-6xl 2xl:text-8xl mt-3">Travel&Co</h2>
         </h1>
@@ -24,18 +24,18 @@
       </div>
     </div>
 
-      <div id="section3" class="pb-24">
-        <div class="top">
+      <div id="section3" class="lg:pb-24 sm:pb-6">
+        <div class="section3-top">
           <h1 class="text-3xl text-gray-700 font-extrabold">Nos voyages du moment</h1>
           <p class="pl-2 pr-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
             aperiam! Commodi itaque necessitatibus autem asperiores! Minus natus
             dicta modi. Eius.
           </p>
-          <div id="separator"></div>
+          <div id="section3-separator"></div>
         </div>
-        <div class="bottom">
-          <div v-for="(card, index) in cards" :key="index" class="card">
+        <div class="section3-bottom sm:grid-cols-1 sm:grid-rows-3 sm:grid-flow-row-dense">
+          <div v-for="(card, index) in cards" :key="index" class="section3-card">
             <span v-if="card.new" class="absolute text-white bg-green-500 p-2 ml-3 mt-3 rounded">Nouveau</span>
             <span v-if="card.discount.isActive" class="absolute text-white bg-yellow-500 p-2 ml-3 mt-3 rounded">Réduction</span>
             <img :src="card.img" alt="product image" />
@@ -51,18 +51,22 @@
         </div>
       </div>
 
-      <div class="section4 w-full h-screen flex flex-col">
-        <div class="w-full h-3/6">
+      <div class="section4 w-full sm:h-full lg:h-screen flex flex-col">
+        <div class="section4-top w-full lg:h-3/6 sm:h-full sm:flex-col lg:flex-row">
           <h1 class="text-center mt-10 text-4xl text-gray-700 font-extrabold">Pourquoi choisir Travel&Co ?</h1>
-          <div class="w-full h-96 flex justify-around items-center">
-            <div v-for="(service, index) in services" :key="index" class="service w-96 h-80 m-5 flex flex-col items-center">
-              <img class="w-32 mt-8 transition-all duration-300 ease" :class="{svg : index <= 1, 'animate-spin-slow' : index == 2}" :src="require(`~/assets/${service.img}.svg`)" :alt="service.img +' photo'">
+
+          <div class="w-full lg:h-96 flex justify-around items-center sm:flex-col lg:flex-row">
+            <div v-for="(service, index) in services" :key="index" class="service lg:w-96 sm:w-full h-80 m-5 flex flex-col items-center px-3">
+              <img class="w-32 mt-8 transition-all duration-300 ease" :class="{svg : index <= 1, 'animate-spin-slow' : index == 2}" :src="require(`~/assets/svgs/${service.img}.svg`)" :alt="service.img +' photo'">
               <h1 class="text-center mt-10 text-2xl text-gray-700 font-extrabold">{{service.title}}</h1>
               <p class="text-center mt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque tempora, error ratione vel porro obcaecati?</p>
             </div>
           </div>
+
         </div>
-          <div class="w-full h-3/6 bg-gray-100"></div>
+          <div class="section4-bottom w-full lg:h-3/6 sm:h-full bg-gray-100">
+
+          </div>
       </div>
     </div> 
 </template>
@@ -140,196 +144,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@200;300;400;500&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500&display=swap');
-
-$primaryFont: "Poppins", sans-serif;
-$secondaryFont: "Quicksand", sans-serif;
-
-.home {
-  width: 100%;
-  height: 100%; 
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-
-  a {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-  .section1 {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  z-index: 1;
-  .title{
-    position: relative;
-    z-index: 2;
-  }
-
-  .logo {
-  filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(-20deg)
-  brightness(500%) contrast(200%);
-}
-}
-
-#section3 {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  background-color: whitesmoke; 
-.top {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-  width: 500px;
-  height: 25%;
-  margin-top: 40px;
-}
-
-p {
-  margin-top: 5px;
-  font-family: $secondaryFont;
-  font-size: 16px;
-  text-align: center;
-}
-
-span {
-  font-family: $primaryFont;
-}
-
-#separator {
-  width: 200px;
-  height: 2px;
-  background-color: rgb(0, 162, 255);
-  margin-top: 20px;
-  margin-bottom: 20px;
-}
-
-.bottom {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-}
-
-.card {
-  width: 500px;
-  height: 700px;
-  margin: 20px;
-  background-color: #fff;
-  border-radius: 15px;
-  -webkit-box-shadow: 12px 7px 12px -2px rgba(0,0,0,0.42);
-  -moz-box-shadow: 12px 7px 12px -2px rgba(0,0,0,0.42);
-  box-shadow: 12px 7px 12px -2px rgba(0,0,0,0.42);
-}
-
-.card > img {
-  border-radius: 15px 15px 0px 0px;
-  width: 100%;
-  height: 75%;
-  object-fit: cover;
-
-}
-
-@media (max-width: 860px) {
-  .section4{
-    height: 100%;
-  }
-  .bottom {
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-    grid-template-rows: repeat(3, 750px);
-    grid-auto-flow: row dense;
-  }
-  .card {
-    width: 350px;
-    margin: 0 auto;
-    height: 600px;
-  }
-  .card > img {
-    height: 70%;
-  }
-  .top {
-    width: 375px;
-  }
-  #section3 {
-    height: 100%;
-  }
-}
-}
-
-}
-
-h1,h2,h3,button {
-  font-family: $primaryFont;
-}
-
-button:hover{
-  background-color: #db3f3f;
-  transition: all 0.3s ease;
-}
-
-
-.star-ratings-css {
-  unicode-bidi: bidi-override;
-  font-size: 25px;
-  height: 25px;
-  width: 100px;
-  margin: 0 auto;
-  position: relative;
-} 
-.star-ratings-css::before { 
-  content: '★★★★★';
-  opacity: .3;
-}
-
-[title=".000"]::after {
-  width: 0%;
-}
-[title=".125"]::after {
-  width: 12.5%;
-}
-[title=".250"]::after {
-  width: 25%;
-}
-[title=".375"]::after {
-  width: 37.5%;
-}
-[title=".500"]::after {
-  width: 50%;
-}
-[title=".625"]::after {
-  width: 62.5%;
-}
-[title=".720"]::after {
-  width: 72%;
-}
-[title=".875"]::after {
-  width: 87.5%;
-}
-.star-ratings-css::after {
-  color: gold;
-  content: '★★★★★';
-  position: absolute;
-  z-index: 1;
-  display: block;
-  left: 0;
-  top:0;
-  width: attr(rating);
-  overflow: hidden;
-}
-
-  .svg:hover{
-  transition: all 0.3s ease;
-    filter: invert(48%) sepia(50%) saturate(2476%) hue-rotate(28deg)
-  brightness(100%) contrast(200%);
-}
+@import '../assets/scss/index.scss';
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       animation: {
         'spin-slow': 'spin 4s linear infinite',
